40acb7ec15de3df24b275258eddfc10a1a12f984
Andrea Mazzon <mazzon@math.lmu.de>
2022-05-05 06:18:30 +0200

    Numerical methods repository initialization:firs handout code

---
+++ src/main/java/com/andreamazzon/handout1/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package hosts the code for the solution of the first exercise sheet. In
+ * particular, the topic of Computer Arithmetics is treated.
+ */
+package com.andreamazzon.handout1;
