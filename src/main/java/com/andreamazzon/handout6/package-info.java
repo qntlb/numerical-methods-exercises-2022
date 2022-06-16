447505eacd1f10a8e070a2b8fe53fc4b76b96919
Andrea Mazzon <mazzon@math.lmu.de>
2022-06-16 22:28:16 +0200

    Code for handout 6

---
+++ src/main/java/com/andreamazzon/handout6/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The code in this package deals with the generation of samples of a random
+ * variable of given distribution, and with some methods that investigate the
+ * properties of the random variable into consideration.
+ */
+package com.andreamazzon.handout6;
